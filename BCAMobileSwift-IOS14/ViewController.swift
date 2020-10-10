e55f6b01cd7126aed526f4c07f946b3879837eba
Jlius Suweno <jliuss@live.com>
2020-10-10 16:12:32 +0700

    Initial Commit

---
+++ BCAMobileSwift-IOS14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BCAMobileSwift-IOS14
+//
+//  Created by Jlius Suweno on 10/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
